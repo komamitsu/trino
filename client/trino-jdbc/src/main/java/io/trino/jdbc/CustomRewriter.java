cf43ff2d59331f42525deae81b5ec3c33b5bde25
Mitsunori Komatsu <komamitsu@gmail.com>
2022-05-11 21:29:44 +0900

    Use Babel parser

--- client/trino-jdbc/src/main/java/io/trino/jdbc/CustomRewriter.java
+++ client/trino-jdbc/src/main/java/io/trino/jdbc/CustomRewriter.java
@@ -11,12 +11,13 @@ import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlDataTypeSpec;
 import org.apache.calcite.sql.SqlFunctionCategory;
 import org.apache.calcite.sql.SqlIdentifier;
+import org.apache.calcite.sql.SqlIntervalQualifier;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlLiteral;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
-import org.apache.calcite.sql.SqlNumericLiteral;
 import org.apache.calcite.sql.SqlOperatorTable;
+import org.apache.calcite.sql.SqlTimestampLiteral;
 import org.apache.calcite.sql.SqlTypeNameSpec;
 import org.apache.calcite.sql.SqlUnresolvedFunction;
 import org.apache.calcite.sql.SqlUserDefinedTypeNameSpec;
@@ -27,15 +28,18 @@ import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.sql.parser.SqlParserUtil;
+import org.apache.calcite.sql.parser.babel.SqlBabelParserImpl;
 import org.apache.calcite.sql.util.SqlShuttle;
-import org.apache.calcite.sql.validate.SqlValidator;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.sql.validate.SqlValidatorCatalogReader;
 import org.apache.calcite.sql.validate.SqlValidatorImpl;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.Planner;
+import org.apache.calcite.util.TimestampString;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -63,6 +67,58 @@ public class CustomRewriter
     private static class SqlVisitor
             extends SqlShuttle
     {
+        private SqlNode parseTimeStampValue(SqlNode node, SqlParserPos pos)
+        {
+            SqlNode newNode;
+            if (node.getKind() == SqlKind.LITERAL) {
+                String s = node.toString();
+                try {
+                    newNode = SqlTimestampLiteral.createTimestamp(
+                            TimestampString.fromMillisSinceEpoch(
+                                    // TODO: Optimize
+                                    SqlParserUtil.parseInteger(s).multiply(new BigDecimal("1000")).longValue()), 0, pos);
+                }
+                catch (NumberFormatException e) {
+                    newNode = visit(SqlParserUtil.parseTimestampLiteral(node.toString(), pos));
+                }
+            }
+            else {
+                newNode = new SqlBasicCall(
+                        new SqlCastFunction(),
+                        (SqlNodeList) visit(SqlNodeList.of(node, new SqlDataTypeSpec(new SqlUserDefinedTypeNameSpec("TIMESTAMP", pos), pos))),
+                        pos);
+            }
+            return newNode;
+        }
+
+        private SqlLiteral parseTimeunit(SqlNode node, SqlParserPos pos)
+        {
+            String timeunit;
+            String origIdentifier = ((SqlIntervalQualifier)node).timeUnitRange.toString().toLowerCase();
+            if (origIdentifier.equals("yy") || origIdentifier.equals("yyyy")) {
+                timeunit = "year";
+            }
+            else if (origIdentifier.equals("mm") || origIdentifier.equals("m")) {
+                timeunit = "month";
+            }
+            else if (origIdentifier.equals("dd") || origIdentifier.equals("d")) {
+                timeunit = "day";
+            }
+            else if (origIdentifier.equals("hh")) {
+                timeunit = "hour";
+            }
+            else if (origIdentifier.equals("mi") || origIdentifier.equals("n")) {
+                timeunit = "minute";
+            }
+            else if (origIdentifier.equals("ss") || origIdentifier.equals("s")) {
+                timeunit = "second";
+            }
+            else {
+                timeunit = origIdentifier;
+            }
+            return SqlLiteral.createCharString(timeunit, pos);
+        }
+
         @Override
         public @Nullable SqlNode visit(SqlCall call)
         {
@@ -82,60 +138,28 @@ public class CustomRewriter
                             pos);
                 }
                 case OTHER_FUNCTION: {
-                    if (call.getOperator().getName().equalsIgnoreCase("DATEADD")) {
-                        String timeunit;
-                        String origIdentifier = ((SqlIdentifier)call.getOperandList().get(0)).getSimple().toLowerCase();
-                        if (origIdentifier.equals("yy") || origIdentifier.equals("yyyy")) {
-                            timeunit = "year";
-                        }
-                        else if (origIdentifier.equals("mm") || origIdentifier.equals("m")) {
-                            timeunit = "month";
-                        }
-                        else if (origIdentifier.equals("dd") || origIdentifier.equals("d")) {
-                            timeunit = "day";
-                        }
-                        else if (origIdentifier.equals("hh")) {
-                            timeunit = "hour";
-                        }
-                        else if (origIdentifier.equals("mi") || origIdentifier.equals("n")) {
-                            timeunit = "minute";
-                        }
-                        else if (origIdentifier.equals("ss") || origIdentifier.equals("s")) {
-                            timeunit = "second";
-                        }
-                        else {
-                            timeunit = origIdentifier;
-                        }
-
-                        SqlNode origDiff = call.operand(1);
-                        SqlNode diff;
-                        if (origDiff.getKind() == SqlKind.LITERAL) {
-                            diff = visit((SqlNumericLiteral)origDiff);
-                        }
-                        else {
-                            diff = visit((SqlBasicCall)origDiff);
-                        }
-
-                        SqlNode origTarget = call.operand(2);
-                        SqlNode target;
-                        if (origTarget.getKind() == SqlKind.LITERAL) {
-                            target = visit(SqlParserUtil.parseTimestampLiteral(origTarget.toString(), pos));
-                        }
-                        else {
-                            target = visit((SqlBasicCall)origTarget);
-                        }
+                    String funcName = call.getOperator().getName();
+                    if (funcName.equalsIgnoreCase("DATEADD")) {
+                        SqlLiteral timeunit = parseTimeunit(call.operand(0), pos);
+                        SqlNode diff = parseTimeStampValue(call.operand(1), pos);
+                        SqlNode target = parseTimeStampValue(call.operand(2), pos);
                         return new SqlBasicCall(
                                 new SqlUnresolvedFunction(
                                         new SqlIdentifier("DATE_ADD", pos),
                                         null, null, null, null, SqlFunctionCategory.TIMEDATE
                                 ),
-                                SqlNodeList.of(
-                                        SqlLiteral.createCharString(timeunit, pos),
-                                        diff,
-                                        target
+                                SqlNodeList.of(timeunit, diff, target), pos);
+                    }
+                    else if (funcName.equalsIgnoreCase("DATEDIFF")) {
+                        SqlLiteral timeunit = parseTimeunit(call.operand(0), pos);
+                        SqlNode v1 = parseTimeStampValue(call.operand(1), pos);
+                        SqlNode v2 = parseTimeStampValue(call.operand(2), pos);
+                        return new SqlBasicCall(
+                                new SqlUnresolvedFunction(
+                                        new SqlIdentifier("DATE_DIFF", pos),
+                                        null, null, null, null, SqlFunctionCategory.TIMEDATE
                                 ),
-                                pos
-                        );
+                                SqlNodeList.of(timeunit, v1, v2), pos);
                     }
                     break;
                 }
@@ -148,11 +172,9 @@ public class CustomRewriter
         SchemaPlus schema = Frameworks.createRootSchema(true);
         FrameworkConfig config = Frameworks.newConfigBuilder()
                 .defaultSchema(schema)
-                .sqlValidatorConfig(SqlValidator.Config.DEFAULT)
                 .parserConfig(
-                        SqlParser.config()
-                                //   .withQuoting(Quoting.BRACKET)
-                                .withCaseSensitive(true)
+                        SqlParser.Config.DEFAULT.withParserFactory(SqlBabelParserImpl.FACTORY)
+                                .withConformance(SqlConformanceEnum.BABEL)
                                 .withQuotedCasing(Casing.UNCHANGED)
                                 .withUnquotedCasing(Casing.UNCHANGED)
                 ).build();
@@ -181,7 +203,11 @@ public class CustomRewriter
                 // TODO: Fix this naive way
                 sql.replace("[", "").replace("]", "")
                         // TODO: Remove this after https://github.com/apache/calcite/pull/2795 is merged
-                        .replace("DAY(", "DAY_OF_YEAR(")
+                        // .replace("DAY(", "DAY_OF_YEAR(")
+
+                        // TODO: Fix this naive way
+                        .replace("(S,", "(second,")
+                        .replace("(s,", "(second,")
         );
         RelDataTypeFactory typeFactory = new JavaTypeFactoryImpl(config.getTypeSystem());
         SqlRewriter rewriter = new SqlRewriter(config.getOperatorTable(),
